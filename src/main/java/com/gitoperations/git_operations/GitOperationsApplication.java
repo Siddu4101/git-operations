5a198fd7413787b1378c7c4c8eabf3edfbabdec3
siddu4101 <siddu412001@gmail.com>
2024-07-13 11:01:09 +0530

    first commit

---
+++ src/main/java/com/gitoperations/git_operations/GitOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.gitoperations.git_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitOperationsApplication.class, args);
+	}
+
+}
